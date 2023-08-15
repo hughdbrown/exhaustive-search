c91d37f642ee39ef00c33b6605d02263ceddbdf0
Hugh Brown <hughdbrown@yahoo.com>
2023-08-15 10:41:27 -0600

    Changed return type of exhaustive_search, changed tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,50 @@
-use std::cmp::max;
-
-pub struct Item {
-    pub value: u64,
-    pub weight: usize,
-}
+use knapsack_utils::Item;
 
 pub enum SearchResult {
-    Success(u64),
+    Success((Vec<usize>, usize, u64)),
     Failure,
 }
 
-fn exhaustive_search_helper(items: &[Item], i: usize, limit_weight: usize, weight: usize, value: u64) -> SearchResult {
+fn exhaustive_search_helper(
+    items: &[Item],
+    i: usize,
+    limit_weight: usize,
+    weight: usize,
+    value: u64,
+    path: &[usize],
+) -> SearchResult {
     if weight > limit_weight {
         return SearchResult::Failure;
     }
     if i >= items.len() {
-        return SearchResult::Success(value);
+        return SearchResult::Success((path.to_vec(), weight, value));
     }
+
     let item = &items[i];
-    let left: SearchResult = exhaustive_search_helper(items, i + 1, limit_weight, weight, value);
-    let right: SearchResult = exhaustive_search_helper(items, i + 1, limit_weight, weight + item.weight, value + item.value);
+    let mut rpath: Vec<usize> = path.to_vec();
+    rpath.push(i);
+    let left: SearchResult = exhaustive_search_helper(items, i + 1, limit_weight, weight, value, path);
+    let right: SearchResult = exhaustive_search_helper(items, i + 1, limit_weight, weight + item.weight, value + item.value, &rpath);
     match (left, right) {
-        (SearchResult::Success(lvalue), SearchResult::Success(rvalue)) => return SearchResult::Success(max(lvalue, rvalue)),
+        (SearchResult::Success(lvalue), SearchResult::Success(rvalue)) => {
+            if lvalue.2 > rvalue.2 {
+                return SearchResult::Success(lvalue);
+            }
+            else {
+                return SearchResult::Success(rvalue);
+            }
+        },
         (SearchResult::Success(lvalue), SearchResult::Failure) => return SearchResult::Success(lvalue),
         (SearchResult::Failure, SearchResult::Success(rvalue)) => return SearchResult::Success(rvalue),
         (SearchResult::Failure, SearchResult::Failure) => return SearchResult::Failure,
     }
 }
 
-pub fn exhaustive_search(items: &[Item], limit_weight: usize) -> u64 {
-    match exhaustive_search_helper(items, 0, limit_weight, 0, 0) {
+pub fn exhaustive_search(items: &[Item], limit_weight: usize) -> (Vec<usize>, usize, u64) {
+    let path = vec![];
+    match exhaustive_search_helper(items, 0, limit_weight, 0, 0, &path) {
         SearchResult::Success(value) => return value,
-        SearchResult::Failure => return 0,
+        SearchResult::Failure => return (path, 0, 0),
     }
 }
 
@@ -47,7 +60,7 @@ mod tests {
             Item{value:3000, weight:4},
         ];
         let value = exhaustive_search(&items, 4);
-        assert_eq!(value, 3500);
+        assert_eq!(value.2, 3500);
     }
 
     #[test]
@@ -62,20 +75,20 @@ mod tests {
             Item{value:4400, weight:8},
         ];
         let value = exhaustive_search(&items, 34);
-        assert_eq!(value, 22400);
+        assert_eq!(value.2, 22400);
         let value = exhaustive_search(&items, 33);
-        assert_eq!(value, 20900);
+        assert_eq!(value.2, 20900);
         let value = exhaustive_search(&items, 32);
-        assert_eq!(value, 20400);
+        assert_eq!(value.2, 20400);
         let value = exhaustive_search(&items, 31);
-        assert_eq!(value, 20400);
+        assert_eq!(value.2, 20400);
         let value = exhaustive_search(&items, 30);
-        assert_eq!(value, 19400);
+        assert_eq!(value.2, 19400);
         let value = exhaustive_search(&items, 29);
-        assert_eq!(value, 19100);
+        assert_eq!(value.2, 19100);
         let value = exhaustive_search(&items, 28);
-        assert_eq!(value, 18400);
+        assert_eq!(value.2, 18400);
         let value = exhaustive_search(&items, 27);
-        assert_eq!(value, 18200);
+        assert_eq!(value.2, 18200);
     }
 }
