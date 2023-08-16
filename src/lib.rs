17f8d109bf8f5e3e5844a9784aa00f1f1e934025
Hugh Brown <hughdbrown@yahoo.com>
2023-08-16 10:35:40 -0600

    Change exhaustive_search code to use Result<> idiom

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 use knapsack_utils::Item;
 
-pub enum SearchResult {
-    Success((Vec<usize>, usize, u64)),
-    Failure,
-}
+type SearchResult =  (Vec<usize>, usize, u64);
 
 fn exhaustive_search_helper(
     items: &[Item],
@@ -12,40 +9,38 @@ fn exhaustive_search_helper(
     weight: usize,
     value: u64,
     path: &[usize],
-) -> SearchResult {
+) -> Result<SearchResult, ()>
+{
     if weight > limit_weight {
-        return SearchResult::Failure;
+        return Err(()); 
     }
     if i >= items.len() {
-        return SearchResult::Success((path.to_vec(), weight, value));
+        return Ok((path.to_vec(), weight, value));
     }
 
     let item = &items[i];
     let mut rpath: Vec<usize> = path.to_vec();
     rpath.push(i);
-    let left: SearchResult = exhaustive_search_helper(items, i + 1, limit_weight, weight, value, path);
-    let right: SearchResult = exhaustive_search_helper(items, i + 1, limit_weight, weight + item.weight, value + item.value, &rpath);
+    let left = exhaustive_search_helper(items, i + 1, limit_weight, weight, value, path);
+    let right = exhaustive_search_helper(items, i + 1, limit_weight, weight + item.weight, value + item.value, &rpath);
     match (left, right) {
-        (SearchResult::Success(lvalue), SearchResult::Success(rvalue)) => {
+        (Ok(lvalue), Ok(rvalue)) => {
             if lvalue.2 > rvalue.2 {
-                return SearchResult::Success(lvalue);
+                return Ok(lvalue);
             }
             else {
-                return SearchResult::Success(rvalue);
+                return Ok(rvalue);
             }
         },
-        (SearchResult::Success(lvalue), SearchResult::Failure) => return SearchResult::Success(lvalue),
-        (SearchResult::Failure, SearchResult::Success(rvalue)) => return SearchResult::Success(rvalue),
-        (SearchResult::Failure, SearchResult::Failure) => return SearchResult::Failure,
+        (Ok(lvalue), Err(())) => return Ok(lvalue),
+        (Err(()), Ok(rvalue)) => return Ok(rvalue),
+        (Err(()), Err(())) => return Err(()),
     }
 }
 
-pub fn exhaustive_search(items: &[Item], limit_weight: usize) -> (Vec<usize>, usize, u64) {
+pub fn exhaustive_search(items: &[Item], limit_weight: usize) -> Result<SearchResult, ()> {
     let path = vec![];
-    match exhaustive_search_helper(items, 0, limit_weight, 0, 0, &path) {
-        SearchResult::Success(value) => return value,
-        SearchResult::Failure => return (path, 0, 0),
-    }
+    return exhaustive_search_helper(items, 0, limit_weight, 0, 0, &path);
 }
 
 #[cfg(test)]
@@ -59,8 +54,10 @@ mod tests {
             Item{value:2000, weight:3},
             Item{value:3000, weight:4},
         ];
-        let value = exhaustive_search(&items, 4);
-        assert_eq!(value.2, 3500);
+        match exhaustive_search(&items, 4) {
+            Ok(value) => assert_eq!(value.2, 3500),
+            Err(_) => assert!(false),
+        }
     }
 
     #[test]
@@ -74,21 +71,12 @@ mod tests {
             Item{value:4200, weight:7},
             Item{value:4400, weight:8},
         ];
-        let value = exhaustive_search(&items, 34);
-        assert_eq!(value.2, 22400);
-        let value = exhaustive_search(&items, 33);
-        assert_eq!(value.2, 20900);
-        let value = exhaustive_search(&items, 32);
-        assert_eq!(value.2, 20400);
-        let value = exhaustive_search(&items, 31);
-        assert_eq!(value.2, 20400);
-        let value = exhaustive_search(&items, 30);
-        assert_eq!(value.2, 19400);
-        let value = exhaustive_search(&items, 29);
-        assert_eq!(value.2, 19100);
-        let value = exhaustive_search(&items, 28);
-        assert_eq!(value.2, 18400);
-        let value = exhaustive_search(&items, 27);
-        assert_eq!(value.2, 18200);
+        let expected_vals: Vec<u64> = vec![22400, 20900, 20400, 20400, 19400, 19100, 18400, 18200];
+        for (i, expected_val) in expected_vals.iter().map(|x: &u64| *x).enumerate() {
+            match exhaustive_search(&items, 34 - i) {
+                Ok(value) => assert_eq!(value.2, expected_val),
+                Err(_) => assert!(false),
+            }
+        }
     }
 }
